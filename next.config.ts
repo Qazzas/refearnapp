8164c89c43a063c755b10ec35ef95ad91e984205
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2026-01-10 15:46:44 +0300

    feat: add allowed origins for experimental server actions in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   trailingSlash: false,
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "refearnapp.com",
+        "www.refearnapp.com",
+        "origin.refearnapp.com",
+      ],
+    },
+  },
 }
 
 export default nextConfig
