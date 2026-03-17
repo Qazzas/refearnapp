a0506eedc70d9dc4645a2f5227bbc1bfc24f440e
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2026-03-17 16:24:07 +0300

    feat: add initial migration file and journal entry for marvelous barracuda

--- apps/dashboard/migrations/0005_odd_carlie_cooper.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "license_keys" ADD COLUMN "license_id" text;--> statement-breakpoint
-ALTER TABLE "license_keys" ADD CONSTRAINT "license_keys_license_id_unique" UNIQUE("license_id");
--- apps/dashboard/migrations/0006_lethal_invaders.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "license_keys" RENAME COLUMN "license_id" TO "polar_id";--> statement-breakpoint
-ALTER TABLE "license_keys" DROP CONSTRAINT "license_keys_license_id_unique";--> statement-breakpoint
-ALTER TABLE "license_keys" ADD CONSTRAINT "license_keys_polar_id_unique" UNIQUE("polar_id");
