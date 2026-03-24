8b4f568cf4618f8a72ad9baf5bd0759f34f3f213
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2026-03-24 07:56:27 +0300

    fix: fixed upstash workflow sync batch

--- apps/dashboard/src/app/(organization)/(auth)/login/page.tsx
+++ apps/dashboard/src/app/(organization)/(auth)/login/page.tsx
@@ -1,13 +1,14 @@
+"use client"
 import React from "react"
 import Login from "@/components/pages/Login"
 import type { Metadata } from "next"
 import { buildMetadata } from "@/util/BuildMetadata"
-export const metadata: Metadata = buildMetadata({
-  title: "RefearnApp | Login Page",
-  description: "Login Page",
-  url: "https://refearnapp.com/login",
-  indexable: false,
-})
+// export const metadata: Metadata = buildMetadata({
+//   title: "RefearnApp | Login Page",
+//   description: "Login Page",
+//   url: "https://refearnapp.com/login",
+//   indexable: false,
+// })
 const loginPage = async () => {
   return (
     <>
